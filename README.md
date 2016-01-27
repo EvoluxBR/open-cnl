c7f992e58039c1d14d06aa0e15a143380f9967c4
Victor Torres <vpaivatorres@gmail.com>
2016-01-26 22:58:06 -0300

    Adicionando README e fazendo funcionar com o banco de dados em memória.

---
+++ README.md
@@ -0,0 +1,37 @@
+# OpenCNL
+
+Biblioteca para ler e consultar o banco de dados CNL (Código Nacional de Localidade) seguindo especificações da ANATEL (Agência Nacional de Telecomunicações).
+
+## Exemplo
+
+- Prefeitura Municipal de Natal/RN - (84) 3211-8243
+- Prefeitura Municipal de Parnamirim/RN - (84) 3644-8100
+- Prefeitura Municipal de Mossoró/RN - (84) 3315-4935
+- Prefeitura Municipal de São Paulo/SP - (11) 3124-5100
+
+```python
+from open_cnl.open_cnl import OpenCNL
+
+# Natal/RN - Centro de custo
+cnl = OpenCNL('~/CE_F_130250.TXT', '8432118243')
+
+# Parnamirim/RN - Área conurbada
+# >>> 'VC1'
+cnl.buscar_numero('8436448100')
+
+# Mossoró/RN - Mesmo estado
+# >>> 'VC2'
+cnl.buscar_numero('8433154935')
+
+# Parnamirim/RN - Área conurbada
+# >>> 'VC3'
+cnl.buscar_numero('1131245100')
+```
+
+## Como funciona
+
+O código processa a base de dados em TXT para um banco de dados SQLite3 em memória RAM. O programa ocupa em média 35MB de RAM e apresenta resultados bastante rápidos.
+
+## Autor
+
+Victor Torres <vpaivatorres@gmail.com>
--- open_cnl/open_cnl.py
+++ open_cnl/open_cnl.py
@@ -13,76 +13,91 @@ class OpenCNL(object):
         """
         Lê o arquivo da base e guarda na memória e salva o número de referência.
         """
+        self.conn = sqlite3.connect(':memory:')
+        c = self.conn.cursor()
+        c.execute("""
+            CREATE TABLE `open_cnl` (
+                sigla_uf TEXT,
+                sigla_cnl TEXT,
+                codigo_cnl TEXT,
+                nome_da_localidade TEXT,
+                nome_do_municipio TEXT,
+                codigo_da_area_de_tarifacao TEXT,
+                prefixo TEXT,
+                prestadora TEXT,
+                numero_da_faixa_inicial TEXT,
+                numero_da_faixa_final TEXT,
+                latitude TEXT,
+                hemisferio TEXT,
+                longitude TEXT,
+                sigla_cnl_da_area_local TEXT
+            );
+        """)
+
         base_file = open(base_path, 'r')
-        self.lines = list()
         for line in base_file.readlines():
-            processed = self.process_line(line.decode('latin-1'))
-            self.lines.append(processed)
+            processed_line = self.process_line(line.decode('latin-1'))
+            c.execute("""
+                INSERT INTO open_cnl VALUES (
+                    ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+                );
+            """, processed_line)
 
         base_file.close()
-
-        conn = sqlite3.connect(':memory:')
-        c = conn.cursor()
+        prefix = base_number[:-4]
+        self.number_info = c.execute(
+            """SELECT * FROM open_cnl WHERE prefixo = ?;""", (prefix,)).fetchone()
 
     def process_complete_line(self, line):
         """
-        Processa uma linha em um dicionário de acordo com as especificações.
+        Processa uma linha em uma tupla de acordo com as especificações.
         """
-        processed_line = dict(
-            sigla_uf=line[:2].strip(),
-            sigla_cnl=line[2:6].strip(),
-            codigo_cnl=line[6:11].strip(),
-            nome_da_localidade=line[11:61].strip().replace('  ', ''),
-            nome_do_municipio=line[61:111].strip(),
-            codigo_da_area_de_tarifacao=line[111:116].strip(),
-            prefixo=line[116:123].strip(),
-            prestadora=line[123:153].strip(),
-            numero_da_faixa_inicial=line[153:157].strip(),
-            numero_da_faixa_final=line[157:161].strip(),
-            latitude=self.process_coordinate(line[161:169].strip()),
-            hemisferio=line[169:174].strip(),
-            longitude=self.process_coordinate(line[174:182].strip()),
-            sigla_cnl_da_area_local=line[182:186].strip()
+        processed_line = (
+            line[:2].strip(), # sigla_uf
+            line[2:6].strip(), # sigla_cnl
+            line[6:11].strip(), # codigo_cnl
+            line[11:61].strip().replace('  ', ''), # nome_da_localidade
+            line[61:111].strip(), # nome_do_municipio
+            line[111:116].strip(), # codigo_da_area_de_tarifacao
+            line[116:123].strip(), # prefixo
+            line[123:153].strip(), # prestadora
+            line[153:157].strip(), # numero_da_faixa_inicial
+            line[157:161].strip(), # numero_da_faixa_final
+            self.process_coordinate(line[161:169].strip()), # latitude
+            line[169:174].strip(), # hemisferio
+            self.process_coordinate(line[174:182].strip()), # longitude
+            line[182:186].strip() # sigla_cnl_da_area_local
         )
         return processed_line
 
     def process_incomplete_line(self, line):
         """
-        Processa uma linha em um dicionário de acordo com as especificações.
+        Processa uma linha em uma tupla de acordo com as especificações.
         """
-        processed_line = dict(
-            sigla_uf=line[:2].strip(),
-            sigla_cnl=line[2:6].strip(),
-            codigo_cnl=line[6:11].strip(),
-            nome_da_localidade=line[11:61].strip().replace('  ', ''),
-            nome_do_municipio=line[61:111].strip(),
-            codigo_da_area_de_tarifacao=line[111:116].strip(),
-            prefixo=line[116:123].strip(),
-            prestadora=line[123:153].strip(),
-            numero_da_faixa_inicial=line[153:157].strip(),
-            numero_da_faixa_final=line[157:161].strip(),
-            sigla_cnl_da_area_local=line[161:164].strip()
+        processed_line = (
+            line[:2].strip(), # sigla_uf
+            line[2:6].strip(), # sigla_cnl
+            line[6:11].strip(), # codigo_cnl
+            line[11:61].strip().replace('  ', ''), # nome_da_localidade
+            line[61:111].strip(), # nome_do_municipio
+            line[111:116].strip(), # codigo_da_area_de_tarifacao
+            line[116:123].strip(), # prefixo
+            line[123:153].strip(), # prestadora
+            line[153:157].strip(), # numero_da_faixa_inicial
+            line[157:161].strip(), # numero_da_faixa_final
+            '', '', '', # latitude, hemisferio, longitude
+            line[161:164].strip() # sigla_cnl_da_area_local
         )
         return processed_line
 
     def process_line(self, line):
         """
-        Processa uma linha em um dicionário de acordo com as especificações.
+        Verifica o tamanho da linha e encaminha para o método adequado.
         """
-        processed_line = dict(
-            sigla_uf=line[:2].strip(),
-            sigla_cnl=line[2:6].strip(),
-            codigo_cnl=line[6:11].strip(),
-            nome_da_localidade=line[11:61].strip().replace('  ', ''),
-            nome_do_municipio=line[61:111].strip(),
-            codigo_da_area_de_tarifacao=line[111:116].strip(),
-            prefixo=line[116:123].strip(),
-            prestadora=line[123:153].strip(),
-            numero_da_faixa_inicial=line[153:157].strip(),
-            numero_da_faixa_final=line[157:161].strip(),
-            sigla_cnl_da_area_local=line[161:164].strip()
-        )
-        return processed_line
+        if len(line) > 167:
+            return self.process_complete_line(line)
+        else:
+            return self.process_incomplete_line(line)
 
     def process_coordinate(self, coordinate):
         """
@@ -93,8 +108,19 @@ class OpenCNL(object):
         CC = Centésimos de segundo.
         Estamos ignorando centésimos de segundo e retornando GG.MMSS (float).
         """
-        return float('%s.%s' % (coordinate[:2], coordinate[2:6]))
+        return '%s.%s' % (coordinate[:2], coordinate[2:6])
 
     def eval_number(self, target_number):
         """Searchs for target number in the base which is stored in memory."""
-        pass
+        prefix = target_number[:-4]
+        c = self.conn.cursor()
+
+        number_info = c.execute(
+            """SELECT * FROM open_cnl WHERE prefixo = ?;""", (prefix,)).fetchone()
+
+        if number_info[5] == self.number_info[5]:
+            return 'VC1'
+        elif number_info[5][0] == self.number_info[5][0]:
+            return 'VC2'
+        else:
+            return 'VC3'
