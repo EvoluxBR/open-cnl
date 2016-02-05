813aa5c4d25e7403ee3ac7cbdf2777052054f702
Victor Torres <vpaivatorres@gmail.com>
2016-02-05 17:03:22 -0300

    Corrigindo descrição no PyPI.

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name = 'open_cnl',
     packages = ['open_cnl'],
-    version = '0.1',
-    description = 'A random test lib',
+    version = '0.2',
+    description = 'Biblioteca para ler e consultar banco de dados CNL (Código Nacional de Localidade) seguindo especificações da ANATEL (Agência Nacional de Telecomunicações).',
     author = 'Victor Torres',
     author_email = 'vpaivatorres@gmail.com',
     url = 'https://github.com/evoluxbr/open-cnl',
-    download_url = 'https://github.com/evoluxbr/open-cnl/tarball/0.1',
+    download_url = 'https://github.com/evoluxbr/open-cnl/tarball/0.2',
     keywords = ['anatel', 'cnl', 'base'],
     classifiers = [],
 )
