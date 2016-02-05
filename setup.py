59584fcc7399a4ebcb6f28e6ae527f25ab66ad6c
Victor Torres <vpaivatorres@gmail.com>
2016-02-05 15:49:11 -0300

    Adicionando setup.py e arquivos auxiliares.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'open_cnl',
+    packages = ['open_cnl'],
+    version = '0.1',
+    description = 'A random test lib',
+    author = 'Victor Torres',
+    author_email = 'vpaivatorres@gmail.com',
+    url = 'https://github.com/evoluxbr/open-cnl',
+    download_url = 'https://github.com/evoluxbr/open-cnl/tarball/0.1',
+    keywords = ['anatel', 'cnl', 'base'],
+    classifiers = [],
+)
